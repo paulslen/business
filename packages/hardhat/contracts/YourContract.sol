5c980531483b93d2f397da4a3c1f09c3b6e28df2
Paul Len <paulstanlen@gmail.com>
2022-11-07 09:09:50 -0500

    Homework 11 done

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -40,19 +40,3 @@ contract YourContract is Ownable {
     return address(this).balance;
   }
 }
-pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
-
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Ownable.sol"; 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract NFTProject is ERC721, Ownable {
-
-  constructor() ERC721("Pauls NFTs", "PN") {}
-
-  function letsMint (address _to, uint256 _id) public onlyOwner {
-    _safeMint(_to, _id);
-  }
-
-}
